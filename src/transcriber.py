8d0f07c7baab71f8ace091013ad8e40dfd070282
Viktor Hasle <vikhasle@icloud.com>
2026-03-14 15:27:06 +0100

    Half way there

--- src/transcriber.py
+++ src/transcriber.py
@@ -1,24 +1,40 @@
 import whisper
+from pyannote.audio import Pipeline
 import sys
 import os
+from typing import Dict
 
 
-def transcribe(model, path) -> str:
-    opt = whisper.DecodingOptions()
-    audio = whisper.pad_or_trim(whisper.load_audio(path))
+def diarization(path: str) -> Dict:
+    pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization')
 
-    mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model. device)
+    dz = pipeline(path)
 
-    return whisper.decode(model, mel, opt).text
+    return dz.speaker_diarization
+
+
+def transcribe(path: str, word_list: [str], **kwargs) -> Dict:
+    model = whisper.load_model("base.en")
+
+    return whisper.transcribe(model, path, verbose=False, *kwargs)
 
 
 def main():
-    model = whisper.load_model("base.en")
-    if len(sys.argv) != 2:
-        print("Usage: python transcribe /path/to/audio.mp3")
+    if len(sys.argv) != 3:
+        print("Usage: python transcribe /path/to/audio.mp3 /path/to/wordlist.txt")
         exit(1)
+
     audio_path = sys.argv[1]
-    print(transcribe(model, audio_path))
+    with open(sys.argv[2], "r") as wl_file:
+        word_list = wl_file.read_lines()
+
+    results = transcribe(audio_path, word_list)
+    raw_transcripts = results['text']
+
+    with open("examples/transcript.txt", "w") as tran_f:
+        tran_f.write(raw_transcripts)
+
+
 
 
 if __name__ == "__main__":
